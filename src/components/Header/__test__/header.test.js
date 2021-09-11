41e0fbcb65ea27a0afbe69b46e1d45d409b98e24
趙子榮 <zhaozirong@zhaozirongdeiMac.local>
2021-09-11 17:59:35 +0800

    fix: Add describe block.

--- src/components/Header/__test__/header.test.js
+++ src/components/Header/__test__/header.test.js
@@ -1,22 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-it('should render same text passed into title prop', () => {
-    render(
-        <Header 
-          title="Todo Header"
-        />
-    );
-    const headingElement = screen.getByRole("heading", { name: /todo header/i });
-    expect(headingElement).toBeInTheDocument();
-});
-
-it('should only render a header element', () => {
-    render(
-        <Header 
-          title="Todo Header"
-        />
-    );
-    const h1Elements = screen.getAllByText(/todo/i);
-    expect(h1Elements.length).toBe(1);
-});
+describe("Header", () => {
+    it('should only render a header element', () => {
+        render(
+            <Header 
+            title="Todo Header"
+            />
+        );
+        const h1Elements = screen.getAllByText(/todo/i);
+        expect(h1Elements.length).toBe(1);
+    });
+})
